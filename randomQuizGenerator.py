29eb02cbdae81a1592fc7effc4428cf4e655c7ec
NickHerrig <neherrig@gmail.com>
2018-12-26 14:22:39 -0600

    adding new project to practice reading/writng files.

---
+++ randomQuizGenerator.py
@@ -0,0 +1,15 @@
+#! python3
+# randomQuizGenerator.py - Creates quizes with questions and answers in 
+# random order, along with the answer key. 
+
+import random
+
+# TODO: store the quiz data in a dictionary
+
+# TODO: Create the quize file and shuffle the question order
+
+# TODO: Create the answer options
+
+# TODO: Write content to the quiz and answer key files
+
+
