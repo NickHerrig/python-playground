ecf06696bd7d20ab8383dbee9e88326e6a732fee
NickHerrig <neherrig@gmail.com>
2019-01-19 12:00:50 -0600

    completed function outline for compairing which player wins or loses

---
+++ rockPaperScissors.py
@@ -0,0 +1,26 @@
+# Python3
+
+playerA = str(input("Rock, Paper, or Scissor?")
+playerB = str(input("Rock, Paper, or Scissor?")
+
+aScore = 0
+bScore = 0
+
+def compareResponse(playerA, playerB):
+	if playerA == playerB:
+		# TODO Tie
+	elif playerA == "Rock":
+		if playerB == "Scissors":
+			# TODO: PlayerA wins
+		elif playerB == "Paper":
+			# TODO: PlayerB wins
+	elif playerA == "Paper":
+		if playerB == "Rock":
+			# TODO: PlayerA wins
+		elif PlayerB == "Scissors":
+			# TODO: PlayerB wins
+	elif playerA == "Scissors":
+		if playerB == "Rock":
+			# TODO: PlayerB wins		
+		elif playerB == "Paper":
+			# TODO: PlayerA wins
