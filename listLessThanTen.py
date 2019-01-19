59629e62b774e8a1185692d658b01844fa14090c
NickHerrig <neherrig@gmail.com>
2019-01-19 00:40:25 -0600

    This practice utilizes Lists and Conditionals

---
+++ listLessThanTen.py
@@ -0,0 +1,14 @@
+
+userNumber = int(input("please enter a number: "))
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+b = []
+
+for num in a:
+	if num < userNumber:
+		b.append(num)
+
+print(b)
+
+
