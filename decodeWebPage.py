f42cb1a87a0dacc51987eae8e3a973f388b52615
NickHerrig <neherrig@gmail.com>
2019-01-21 00:46:01 -0600

    updated code to create a brand/price dictionary of competition tires

--- decodeWebPage.py
+++ decodeWebPage.py
@@ -3,8 +3,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.nytimes.com/section/business'
+
+url = 'https://deltawholesaletire.com/prices/'
 r = requests.get(url)
 r_html = r.text
 
+
 soup = BeautifulSoup(r_html, features="html.parser")
+
+
+brandList = [head.text for head in soup.find_all(class_='model column')]
+priceList = [head.text for head in soup.find_all(class_='price column')]
+
+while 'PRICE/ EA. TIRE' in priceList:
+	priceList.remove('PRICE/ EA. TIRE')
+priceList.remove('PRICE/ EA. PART')
+
+priceBrandDict = dict(zip(brandList, priceList))
+print(priceBrandDict)
