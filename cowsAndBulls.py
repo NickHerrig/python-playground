bf28159bffce4f1b7c0a6777e19041709fe7b8b4
NickHerrig <neherrig@gmail.com>
2019-01-21 19:47:46 -0600

    started Cow Bulle game

---
+++ cowsAndBulls.py
@@ -0,0 +1,8 @@
+# Python3
+# Cows and Bulls game which utilizes a main method, randomness with the random module, and functions
+
+def cowAndBull(num):
+	# TODO: Checks for Bulls
+	# TODO: Checks for Cows
+	# TODO: returns count
+
