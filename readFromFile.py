d9b37de01bddd9b6b7138dd5a2156af1aabaca3c
NickHerrig <neherrig@gmail.com>
2019-01-27 20:44:12 -0600

    read files using the read file and line

---
+++ readFromFile.py
@@ -0,0 +1,9 @@
+# Python3
+# Read a file and returns how many of each name there is
+
+with open('namesList.txt', 'r') as open_file:
+	all_text = open_file.read()
+	# TODO: initialize a counter dictionary
+
+
+
