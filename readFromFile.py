bd519d08ea89e08db90dcce9f29df90b58ec2d96
NickHerrig <neherrig@gmail.com>
2019-01-28 20:32:00 -0600

    reading a file and adding contents to a dictionary

--- readFromFile.py
+++ readFromFile.py
@@ -1,9 +1,17 @@
 # Python3
 # Read a file and returns how many of each name there is
 
+counter_dict = {}
 with open('namesList.txt', 'r') as open_file:
-	all_text = open_file.read()
-	# TODO: initialize a counter dictionary
+	line = open_file.readline()
+	while line:
+		line = line.strip()
+		if line in counter_dict:
+			counter_dict[line] += 1
+		else:
+			counter_dict[line] = 1
+		line = open_file.readline()
 
 
+print(counter_dict)
 
