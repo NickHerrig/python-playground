eb13850319d11e02d8c4cca82586d245f835a490
NickHerrig <neherrig@gmail.com>
2019-04-09 06:28:18 -0500

    added dockstrings and a shebang for documenting and ease of use

--- pluralsightPython/words.py
+++ pluralsightPython/words.py
@@ -1,12 +1,26 @@
-#  Python3
-#  author: Nick Herrig
+#!/usr/bin/env python3
 
+"""Retrieve and print words from a URL.
+
+Usage:
+
+    python3 words.py <URL>
+"""
 
 import sys
 from urllib.request import urlopen
 
 
 def fetch_words(url):    
+    """Fetch a list of words from a URL.
+       
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from
+        the document.
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -17,15 +31,24 @@ def fetch_words(url):
     
 
 def print_items(items):
+    """Print items on per line.
+
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 
-def main():
-    url = sys.argv[1]
+def main(url):
+    """Print each word from a text document from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
