bd1a65cfe7c86db4884056735e353e04d5bba471
NickHerrig <neherrig@gmail.com>
2019-04-11 06:35:36 -0500

    module to show exceptions, and module to show API exceptions

--- pluralsightPython/exceptional.py
+++ pluralsightPython/exceptional.py
@@ -1,7 +1,21 @@
 
 '''A module for demonstrating exceptions.'''
 
+
+import sys
+from math import log
+
+
 def convert(s):
     '''Convert to an integer.'''
-    x = int(s)
-    return x
+    try:
+        return int(s)
+    except (ValueError, TypeError) as e:
+        print("Conversion Error: {}"\
+              .format(str(e)),
+              file=sys.stderr)
+        raise
+
+def string_log(s):
+    v = convert(s)
+    return log(v)
