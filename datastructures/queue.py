561ca58a468088bd600a8d2c1f25931508d5e92a
Nick Herrig <neherrig@gmail.com>
2019-05-28 21:30:07 -0500

    added classes for experimenting with queues and stacks

---
+++ datastructures/queue.py
@@ -0,0 +1,13 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, value):
+        self.items.insert(0,value)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
--- datastructures/stack.py
+++ datastructures/stack.py
@@ -1,11 +1,12 @@
-class stack:
+class Stack:
     def __init__(self): 
         self.items = []
 
     def push(self, val):
         self.items.append(val)
 
-    def pop (self):
+    def pop(self):
         return self.items.pop()
 
-
+    def size(self):
+        return len(self.items)
