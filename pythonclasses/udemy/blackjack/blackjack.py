93ace88fda6f13ab8e45daef52ec51201ad1b3f4
Nick Herrig <neherrig@gmail.com>
2019-07-02 22:08:22 -0500

    added shuffle and deal methods to deckofcard.py, also added docstrings. increased pylint score to > 8.2, added TODOs to the blackjack game

--- pythonclasses/udemy/blackjack/blackjack.py
+++ pythonclasses/udemy/blackjack/blackjack.py
@@ -1,9 +1,23 @@
 from deckofcards import FrenchDeckOfCards
 from random import randint
 
-deck = FrenchDeckOfCards()
 
-card = deck[randint(1,53)]
+#TODO: Create Deck of cards
 
-print(card)
+#TODO: Shuffle Deck of cards
 
+#TODO: Ask Player for their bet(Cannot be more than current chips)
+
+#TODO: Deal cards to player and dealer (Does the order matter in real blackjack?... Not sure)
+
+#TODO: Ensure one of dealers cards is face up, and show both players cards
+
+#TODO: Ask player if they want to hit or stand
+
+#TODO: If no Bust, Ask if hit or stand
+
+#TODO: Play Dealer Hand (Dealer will always hit until 17 or greater)
+
+#TODO: Determin winner and add/subtract players chips
+
+#TODO: Play Again? 
--- pythonclasses/udemy/blackjack/deckofcards.py
+++ pythonclasses/udemy/blackjack/deckofcards.py
@@ -1,10 +1,18 @@
 import collections
+import random
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeckOfCards:
+    """
+    Purpose: creates a deck of 52 playing cards.
+    Input: None
+    Returns: a list of 52 named tuples Card(rank, suit)
+            rank = 2-11, J, Q, K, A
+            suit = heart, club, spade, or diamond
+    """
 
-    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'heart club spade diamond'.split()
 
     def __init__(self):
@@ -15,3 +23,21 @@ class FrenchDeckOfCards:
 
     def __getitem__(self, position):
         return self._cards[position]
+
+    def shuffle(self):
+        """
+        Purpose: The shuffle method shuffles existing object list of cards.
+        Input: None
+        Returns: None
+        """
+        random.shuffle(self._cards)
+
+    def deal(self):
+        """
+        Purpose: the deal method deals a card from the deck
+        Input: None
+        Returns: returns  one card from the top of the deck
+                 a named tuple Card(rank, suit)
+        """
+        single_card = self._cards.pop()
+        return single_card
