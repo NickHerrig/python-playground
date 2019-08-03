e4c3551f13c717035345c041becab4473372759e
NickHerrig <nickherrig@github.com>
2019-08-03 18:19:05 -0500

    notice the commpalcent comma placement on the last container entry.

---
+++ pythonclasses/pythontricks/commaplacement.py
@@ -0,0 +1,26 @@
+
+list_example = [
+                'blue',
+                'green',
+                'red',
+                'yellow',
+                'green',
+                'white',
+]
+
+set_example = {
+               'blue',
+               'white',
+               'green',
+}
+
+dict_example = {
+                'favorite': 'blue',
+                'lackof': 'white',
+                'grass': 'green',
+}
+
+
+print(list_example, type(list_example))
+print(set_example, type(set_example))
+print(dict_example, type(dict_example))
