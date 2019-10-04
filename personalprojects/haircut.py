a41881a2caa64caf456e222e01d5cdc15fb46a18
nickherrig <neherrig@gmail.com>
2019-10-03 23:55:50 -0500

    added a few things.. make this cleaner

--- personalprojects/haircut.py
+++ personalprojects/haircut.py
@@ -1,16 +1,21 @@
 import requests
-import re
 from pprint import pprint
+import datetime
 
 
-genbook_endpoint = 'https://www.genbook.com/bookings/api/serviceproviders/30230662/services/989056738/resources/989056742?&day=20191130L&view=day'
+barbers_dates_open = {
+    'jordan':'https://www.genbook.com/bookings/api/serviceproviders/30230662/services/989056738/resources/989056742?',
+    'kegan':'https://www.genbook.com/bookings/api/serviceproviders/30352805/services/2098565278/resources/2098565282?',
+}
 
-genbook_response = requests.get(genbook_endpoint)
+
+genbook_response = requests.get(barbers_dates_open['kegan'])
 
 if genbook_response.status_code != 200:
     raise ApiError('Error, Was unable to get data from genbook: {}'.format(genbook_response.status_code))
+dates = genbook_response.json()['dates']
+clean_dates = [ date[:-1] for date in dates]
+apointment_dates = [datetime.datetime(int(date[:4]),int(date[4:6]),int(date[6:]))
+                    for date in clean_dates]
 
-
-pprint(genbook_response.json()['times'])
-
-
+one_week_from_today = datetime.datetime.now() + datetime.timedelta(days=7) 
