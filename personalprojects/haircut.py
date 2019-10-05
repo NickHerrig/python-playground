04ed6166df5f7708ec601f4c6a9589ad52c1d762
nickherrig <neherrig@gmail.com>
2019-10-05 17:49:59 -0500

    reformate, error handling, functions.

--- personalprojects/haircut.py
+++ personalprojects/haircut.py
@@ -1,21 +1,43 @@
-import requests
+from datetime import datetime, timedelta
 from pprint import pprint
-import datetime
 
+import requests
 
-barbers_dates_open = {
+barbers_endpoints = {
     'jordan':'https://www.genbook.com/bookings/api/serviceproviders/30230662/services/989056738/resources/989056742?',
     'kegan':'https://www.genbook.com/bookings/api/serviceproviders/30352805/services/2098565278/resources/2098565282?',
 }
 
 
-genbook_response = requests.get(barbers_dates_open['kegan'])
+def get_available_appointments(barber):
+    try:
+        genbook_response = requests.get(barbers_endpoints[barber])
+        if genbook_response.status_code != 200:
+            print("Unable to reach genbook endpoint")
+            raise SystemExit
+        return genbook_response.json()['dates']
 
-if genbook_response.status_code != 200:
-    raise ApiError('Error, Was unable to get data from genbook: {}'.format(genbook_response.status_code))
-dates = genbook_response.json()['dates']
-clean_dates = [ date[:-1] for date in dates]
-apointment_dates = [datetime.datetime(int(date[:4]),int(date[4:6]),int(date[6:]))
-                    for date in clean_dates]
+    except KeyError:
+        print("No barber: {} in barber endpoints".format(barber))
+        raise SystemExit
 
-one_week_from_today = datetime.datetime.now() + datetime.timedelta(days=7) 
+
+def format_appointments(appointments):
+    return [datetime(int(date[:4]),int(date[4:6]),int(date[6:8])) for date in appointments]
+
+
+def this_weeks_appointments(available_apointments):
+    week_from_today = datetime.today() + timedelta(days=7)
+    return [date for date in available_apointments if date < week_from_today]
+
+
+def main():
+    all_appointments = get_available_appointments('kegan')
+    appointment_dates = format_appointments(all_appointments)
+    current_weeks_appointments = this_weeks_appointments(appointment_dates)
+
+    for appointment in current_weeks_appointments:
+        pprint(appointment.strftime('%b/%d/%Y'))
+
+if __name__ == '__main__':
+    main()
