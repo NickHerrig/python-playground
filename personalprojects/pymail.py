8be5c7b614674176560b36f2a8a7ec727ebff5fa
nickherrig <neherrig@gmail.com>
2020-03-01 22:58:19 -0600

    Add pymail changes

--- personalprojects/pymail.py
+++ personalprojects/pymail.py
@@ -3,22 +3,22 @@ import smtplib
 import ssl
 
 
+sender_email = "nickherrigdeveloper@gmail.com"
+password = os.environ['DEV_EMAIL_PASS']
+port = 465
+
 
 def send_email(send_to, subject, message):
     """Send an email from nickherrigdeveloper@gmail.com.
 
     Keyword arguments:
-    send_to -- the receiver
+    send_to -- the receiver, can be an email or phone number
     subject -- the subject line
     message -- the contents
 
     """
-    sender_email = "nickherrigdeveloper@gmail.com"
 
     email = 'Subject: {}\n\n{}'.format(subject, message)
-    port = 465
-    password = os.environ['DEV_EMAIL_PASS']
-    
     context = ssl.create_default_context()
     
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
