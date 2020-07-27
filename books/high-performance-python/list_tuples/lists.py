43753e1e2083511ffa2cb53650a9558b0cc682d2
nickherrig <neherrig@gmail.com>
2020-07-26 19:58:13 -0500

    binary search with python list function.

--- books/high-performance-python/list_tuples/lists.py
+++ books/high-performance-python/list_tuples/lists.py
@@ -21,8 +21,22 @@ def linear_search(my_array, my_needle):
         if value == my_needle:
             return index
 
+@time_function
 def binary_search(haystack, needle):
-#TODO Implement a binary search algorithm.
+    minimum, maximum = 0, len(haystack)
+
+    while True:
+        if minimum >= maximum:
+            return -1
+        middle = (minimum + maximum) // 2
+
+        if haystack[middle] > needle:
+            maximum = middle
+        elif haystack[middle] < needle:
+            minimum = middle + 1
+        else:
+            return middle
+
 
 def fetch_needle(my_arry, needle_index):
     return my_list[my_index]
@@ -30,7 +44,8 @@ def fetch_needle(my_arry, needle_index):
 
 def main():
 
-    print(small_list.index(1))
+    print(binary_search(large_list, 88))
+    print(binary_search(large_list, 1))
 
 if __name__=="__main__":
     main()
