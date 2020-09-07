68690d57d4f0bef46d3a9bdb1a454e8b97f8f8d7
nickherrig <neherrig@gmail.com>
2020-09-06 20:23:33 -0500

    python program for binary blog

--- personalprojects/bin-to-char.py
+++ personalprojects/bin-to-char.py
@@ -1,12 +1,21 @@
 
+
+def bin_to_char(byte_list):
+    """Accepts a list of binary digits, and returns a sentance"""
+
+    num_list = [ int(byte, 2) for byte in byte_list ]
+    char_list = [ chr(num) for num in num_list]
+    sentance = "".join(char_list)
+
+    return sentance
+
+
 def main():
 
     with open("love-note.txt") as f:
         byte_list = f.read().splitlines()
 
-    num_list = [ int(byte, 2) for byte in byte_list ]
-    char_list = [ chr(num) for num in num_list ]
-    sentance = "".join(char_list)
+    sentance = bin_to_char(byte_list)
 
     print(sentance)
 
