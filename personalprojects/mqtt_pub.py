3b73088be9bf53467314193f0ca7f41813e56527
nickherrig <neherrig@gmail.com>
2020-09-07 20:08:11 -0500

    Test sub and pub to mqtt and prometheus exporter

--- personalprojects/mqtt_pub.py
+++ personalprojects/mqtt_pub.py
@@ -1,3 +1,6 @@
+import os
+import json
+import time
 import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -7,13 +10,19 @@ def on_connect(client, userdata, flags, rc):
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.connect(ip, port, 60)
+mqtt_broker_host = os.environ['MQTT_BROKER_HOST']
+mqtt_broker_port = int(os.environ['MQTT_BROKER_PORT'])
+topic = os.environ['MQTT_TOPIC']
 
-payload = 'hello'
-request = client.publish("/name/message", payload=payload)
+client.connect(mqtt_broker_host, mqtt_broker_port, 60)
 
-print(request.is_published)
-print(request.mid)
-print(request.rc)
-
-
+while True:
+    payload = {"temperature":23.20,"humidity":51.60}
+    payload = json.dumps(payload)
+    request = client.publish(topic=topic, payload=payload)
+    print(request.rc)
+    time.sleep(10)
+    payload = {"temperature":40.0,"humidity":5.60}
+    payload = json.dumps(payload)
+    request = client.publish(topic=topic, payload=payload)
+    time.sleep(10)
--- personalprojects/mqtt_sub.py
+++ personalprojects/mqtt_sub.py
@@ -1,9 +1,12 @@
+import os
+
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    client.subscribe("/name/message")
+    topic = os.environ['MQTT_TOPIC']
+    client.subscribe(topic)
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -12,5 +15,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(ip, port, 60)
+mqtt_broker_host = os.environ['MQTT_BROKER_HOST']
+mqtt_broker_port = int(os.environ['MQTT_BROKER_PORT'])
+client.connect(mqtt_broker_host, mqtt_broker_port, 60)
 client.loop_forever()
