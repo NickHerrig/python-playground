ac2e3ad04304a11ce825e23b04c48270928755c8
nickherrig <neherrig@gmail.com>
2021-02-28 20:07:47 -0600

    Using default dict

--- books/fluentpython/three_dictionaries_and_sets/missingkeys.py
+++ books/fluentpython/three_dictionaries_and_sets/missingkeys.py
@@ -1,23 +1,22 @@
 import sys
 import re
+import collections
 
 
-def main():
+if __name__=="__main__":
     WORD_RE = re.compile(r'\w+')
 
-    index = {}
+    dd = collections.defaultdict(list)
+    nd = {}
+
     with open(sys.argv[1], encoding='utf-8') as f:
         for line_no, line in enumerate(f, 1):
             for match in WORD_RE.finditer(line):
                 word = match.group()
                 column_no = match.start() + 1
                 location = (line_no, column_no)
-                index.setdefault(word, []).append(location)
+                nd.setdefault(word, []).append(location)
+                dd[word].append(location)
 
-    for word in sorted(index, key=str.upper):
-        print(word, index[word])
-
-
-if __name__=="__main__":
-    main()
-
+        print(dd)
+        print(nd)
