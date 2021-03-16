8c0d2402077fb5fc98f64eb5d2a6b265ba25ea32
nickherrig <neherrig@gmail.com>
2021-03-16 13:01:37 -0500

    intro to test normalization

---
+++ books/fluentpython/four_text_versus_bytes/normalization.py
@@ -0,0 +1,16 @@
+from unicodedata import normalize
+
+def main():
+    s1 = 'café'
+    s2 = 'cafe\u0301'
+    print("length 1: ", len(s1), "length 2: ", len(s2))
+    print("length 1: ", len(normalize('NFC', s1)), "length 2: ", len(normalize('NFC', s2)))
+    print(s1 == s2)
+    print(normalize('NFC', s1) == normalize('NFC', s2))
+
+
+
+
+if __name__=="__main__":
+    main()
+
