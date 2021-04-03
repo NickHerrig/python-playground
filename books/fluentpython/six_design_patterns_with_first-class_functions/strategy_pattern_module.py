e34ac3ce77b40a5c8161b5af596c4c89bfde59c0
nickherrig <neherrig@gmail.com>
2021-04-03 10:17:17 -0500

    module introspection and globals()

--- books/fluentpython/six_design_patterns_with_first-class_functions/strategy_pattern_module.py
+++ books/fluentpython/six_design_patterns_with_first-class_functions/strategy_pattern_module.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 from collections import namedtuple
+import inspect
+
+import promotions
 
 Customer = namedtuple('Customer', 'name fidelity')
 
@@ -34,30 +37,8 @@ class Order:
         fmt = '<Order total: {:.2f} due: {:.2f}>'
         return fmt.format(self.total(), self.due())
 
-
-def fidelity_promo(order):
-    """5% discount for customers with 1000 or more fidelity points"""
-    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
-
-
-def bulk_item_promo(order):
-    """10% discount for each LineItem with 20 or more units"""
-    discount = 0
-    for item in order.cart:
-        if item.quantity >= 20:
-            discount += item.total() * .1
-        return discount
-
-def large_order_promo(order):
-    """7% discount for orders with 10 or more distinct items"""
-    distinct_items = {item.product for item in order.cart}
-    if len(distinct_items) >= 10:
-        return order.total() * .07
-    return 0
-
-promos = [globals()[name] for name in globals()
-          if name.endswith("_promo")
-          and name != "best_promo"]
+promos = [func for name, func in
+                inspect.getmembers(promotions, inspect.isfunction)]
 
 def best_promo(order):
     return max(promo(order) for promo in promos)
@@ -71,19 +52,12 @@ def main():
             LineItem('apple', 10, 1.5),
             LineItem('watermellon', 5, 5.0),]
 
-    print(Order(joe, cart, fidelity_promo))
-    print(Order(anna, cart, fidelity_promo))
 
     bannana_cart = [LineItem('banana', 30, .5),
                     LineItem('apple', 10, 1.5),]
 
-    print(Order(joe, bannana_cart, bulk_item_promo))
-
     long_order = [LineItem(str(item_code), 1, 1.0)
                   for item_code in range(10)]
-
-    print(Order(joe, long_order, large_order_promo))
-    print(Order(joe, cart, large_order_promo))
 
     print(Order(joe, long_order, best_promo))
     print(Order(joe, bannana_cart, best_promo))
